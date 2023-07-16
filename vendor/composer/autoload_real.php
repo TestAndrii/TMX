4ff0043557bcb6f12fb58243443a7c10fc4d39ab
Andrii <fav1@ua.fm>
2023-07-16 23:07:01 +0300

    update composer - remove carbon

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit3458aa6c84639eb8e2db69946922975f
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit3458aa6c84639eb8e2db69946922975f', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit3458aa6c84639eb8e2db69946922975f', 'loadClassLoader'));
@@ -32,18 +30,6 @@ class ComposerAutoloaderInit3458aa6c84639eb8e2db69946922975f
         call_user_func(\Composer\Autoload\ComposerStaticInit3458aa6c84639eb8e2db69946922975f::getInitializer($loader));
 
         $loader->register(true);
-
-        $filesToLoad = \Composer\Autoload\ComposerStaticInit3458aa6c84639eb8e2db69946922975f::$files;
-        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-                require $file;
-            }
-        }, null, null);
-        foreach ($filesToLoad as $fileIdentifier => $file) {
-            $requireFile($fileIdentifier, $file);
-        }
 
         return $loader;
     }
